662ea127fe858bd5ec8b6f657038a58b3e074b6c
Kendrick Tan <kendricktan0814@gmail.com>
2016-09-04 13:24:32 +1000

    hlinted

--- easy/easy_276.hs
+++ easy/easy_276.hs
@@ -15,8 +15,8 @@ shitpostFlip n f
 shitpostHorizontal :: Int -> Bool -> String -> String
 shitpostHorizontal n f s 
     | n == 0 = 
-        if f then [head s] ++ "\n"
-        else [last s] ++ "\n"        
+        if f then head s : "\n"
+        else last s : "\n"        
     | n > 0 = 
         if shitpostFlip n f then [s !! x | x <- [0..length s - 2]] ++ shitpostHorizontal (n-1) f s
         else [s !! x | x <- reverse [1..length s - 1]] ++ shitpostHorizontal (n-1) f s
@@ -25,8 +25,8 @@ shitpostHorizontal n f s
 shitpostVertical :: Int -> Int -> Bool -> String -> String
 shitpostVertical c n f s 
     | n == 0 = 
-        if not f then [s !! (length s -1 -c)] ++ "\n"
-        else [s !! c] ++ "\n"        
+        if not f then (s !! (length s -1 -c)) : "\n"
+        else (s !! c) : "\n"        
     | n > 0 =
         if not $ shitpostFlip n f then [s !! (length s - 1 - c)] ++ duplicate (length s - 2) " " ++ shitpostVertical c (n-1) f s
         else [s !! c] ++ duplicate (length s - 2) " " ++ shitpostVertical c (n-1) f s
@@ -36,7 +36,7 @@ shitposting :: Int -> Int -> Int -> Int -> Bool -> String -> String
 shitposting w h cw ch f s
     | ch == 0 = shitpostHorizontal w f s ++ shitposting w h cw (ch+1) f s
     | ch > 0 && ch <= h =
-        if cw == (length s)-1 then shitpostHorizontal w (not f) s ++ shitposting w h 1 (ch+1) (not f) s
+        if cw == length s-1 then shitpostHorizontal w (not f) s ++ shitposting w h 1 (ch+1) (not f) s
         else shitpostVertical cw w f s ++ shitposting w h (cw+1) ch f s
     | ch == (h+1) = "\n"
 
