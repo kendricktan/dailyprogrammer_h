9e6b118d1907c774496533062f9d6c43de57cbbd
kendricktan <kendricktan0814@gmail.com>
2016-12-03 18:45:50 +1000

    293

---
+++ easy/easy_293.hs
@@ -0,0 +1,24 @@
+import Text.Regex.Posix
+import Data.Bool
+import Data.Char
+
+-- Rules
+-- white -> ! white | ! black
+-- red -> green
+-- black -> ! white | ! green | ! orange
+-- orange -> red | black
+-- green -> orange | white
+-- purple -> ! purple | ! green | ! orange ! | white
+bombExplode :: String -> Bool
+bombExplode s = s =~ "w[w|b]|r[^g]|b[w|g|o]|o[^r|^b]|g[^o|^w]|p[p|g|o|w]" :: Bool 
+
+-- Prettify sequence of inputs
+prettifySeq :: [String] -> String
+prettifySeq = map (toLower . head)
+
+main :: IO()
+main = do 
+    let explode = bombExplode $ prettifySeq ["White", "Red", "Green", "white"]
+    if explode 
+        then putStrLn "Bomb exploded"
+    else putStrLn "Bomb diffused safely"
