868875e14ebb138c462e4ffe0a99384b0ea72c8b
kendricktan <kendricktan0814@gmail.com>
2016-12-23 15:47:09 +1000

    sanitized inputs

--- intermediate/intermediate_296.hs
+++ intermediate/intermediate_296.hs
@@ -1,11 +1,7 @@
 data Rectangle = Rectangle Double Double Double Double deriving (Show, Read)
-instance Num Rectangle where
-  (Rectangle x1 y1 x2 y2) + (Rectangle u1 v1 u2 v2) = Rectangle (x1+u1) (y1+v1) (x2+u2) (y2+v2)
-  (Rectangle x1 y1 x2 y2) * (Rectangle u1 v1 u2 v2) = Rectangle (x1*u1) (y1*v1) (x2*u2) (y2*v2)
-  (Rectangle x1 y1 x2 y2) - (Rectangle u1 v1 u2 v2) = Rectangle (x1-u1) (y1-v1) (x2-u2) (y2-v2)
-  abs                       (Rectangle x1 y1 x2 y2) = Rectangle (abs x1) (abs y1) (abs x2) (abs y2)
-  signum                    (Rectangle x1 y1 x2 y2) = Rectangle (signum x1) (signum y1) (signum x2) (signum y2)
-  fromInteger i                                     = Rectangle (fromInteger i) (fromInteger i) (fromInteger i) (fromInteger i)
+
+-- | Remove comma from a string
+removeComma = map (\c -> if c==',' then ' '; else c)
 
 -- | Creates a Rectangle from a list
 makeRect :: [Double] -> Rectangle
@@ -26,7 +22,7 @@ bounds (Rectangle x1 y1 x2 y2) (Rectangle u1 v1 u2 v2)
 
 -- | Converts input into rectangles
 inRectangles :: String -> [Rectangle]
-inRectangles s = map (makeRect . map (read::String->Double) . words) $ lines s
+inRectangles s = map (makeRect . map (read::String->Double) . words) $ lines $ removeComma s
 
 -- | Finds overlap between a list of Rectangles
 boundingRects :: [Rectangle] -> Double
